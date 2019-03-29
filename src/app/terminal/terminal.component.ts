bfca9e799cdedbd4bfa4eee0511e42a3a130637e
tak <njirtak@gmail.com>
2019-03-29 14:49:08 +0900

    feat: add highlight color to tak

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -19,15 +19,27 @@ export class TerminalComponent implements OnInit {
         const speed = 30;
         setTimeout(() => {
             const txt = `env | grep tak
-            NAME=Hyungtak Jin
-            EMAIL=env.tak@gmail.com
+            NAME=Hyung<span class="highlight">t</span><span class="highlight">a</span><span class="highlight">k</span> Jin
+            EMAIL=env.<span class="highlight">t</span><span class="highlight">a</span><span class="highlight">k</span>@gmail.com
             GITHUB=tak-bro
             BLOG=tak-bro.github.io
             RESUME=https://docs.google.com/tak....
             `;
 
-            if (this.index < txt.length) {
-                this.demoTerminal.nativeElement.innerHTML += txt.charAt(this.index);
+
+            const fragments = txt.split(/(\<+[a-zA-Z0-9\=\"\s]+\>+[^<]+\<\/+[a-zA-Z0-9]+\>)/gi);
+            const typingWord = [];
+            fragments.map((word) => {
+                if (word.includes('<span')) {
+                    typingWord.push(word);
+                } else {
+                    word.split('').map(tmp => typingWord.push(tmp));
+                }
+            });
+
+            if (this.index < typingWord.length) {
+                // this.demoTerminal.nativeElement.innerHTML += txt.charAt(this.index);
+                this.demoTerminal.nativeElement.innerHTML += typingWord[this.index]
                 this.index++;
                 setTimeout(() => {
                     this.typeItOut();
