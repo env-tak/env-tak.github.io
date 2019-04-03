027d2f8782ab029dec0b127a36306ea8e2b3ea05
Tak <env.tak@gmail.com>
2019-04-03 18:53:13 +0900

    feat: add default home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { TerminalComponent } from './terminal/terminal.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-      TerminalComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TerminalComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
