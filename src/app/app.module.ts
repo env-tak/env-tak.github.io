bb8624023c56b1b034d8b8eb36c1b3f68b84c548
Tak <env.tak@gmail.com>
2019-08-20 11:28:04 +0900

    fix: delete unused code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MnFullpageModule } from 'ngx-fullpage';
 
 import { FooterComponent } from './core/footer/footer.component';
 import { RegExpService } from './core/services/regexp/regexp.service';
@@ -23,7 +22,6 @@ import { NotFoundComponent } from './modules/not-found/not-found.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        MnFullpageModule.forRoot()
     ],
     providers: [RegExpService],
     bootstrap: [AppComponent]
