0663262c2254ac70e1641c6646c9e1cfcdd45a65
Tak <env.tak@gmail.com>
2019-08-21 10:51:11 +0900

    feat(home): add HomeModule and refactor project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './modules/home/home.component';
-import { NotFoundComponent } from './modules/not-found/not-found.component';
-
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    // not found
-    { path: 'not-found', component: NotFoundComponent, pathMatch: 'full' },
-    { path: '**', redirectTo: 'not-found' },
+    { path: 'home', loadChildren: './modules/home/home.module#HomeModule' },
+    { path: '**', redirectTo: 'home' },
 ];
 
 @NgModule({
