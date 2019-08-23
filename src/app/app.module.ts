f945c3eda7757c55ed9e770148f366e4415d32e0
Tak <env.tak@gmail.com>
2019-08-23 10:39:26 +0900

    Revert "Feature/project structure (#2)"
    This reverts commit e9e16e5a03fb5f9be891c6368e06545b0f292b7f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { CoreModule } from './core/services/core.module';
+import { FooterComponent } from './core/footer/footer.component';
+import { RegExpService } from './core/services/regexp/regexp.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { TerminalComponent } from './modules/terminal/terminal.component';
+import { NotFoundComponent } from './modules/not-found/not-found.component';
+
 
 @NgModule({
     declarations: [
         AppComponent,
+        HomeComponent,
+        TerminalComponent,
+        NotFoundComponent,
+        FooterComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        CoreModule,
     ],
-    providers: [],
+    providers: [RegExpService],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
