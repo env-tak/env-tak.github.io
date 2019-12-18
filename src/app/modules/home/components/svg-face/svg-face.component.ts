32bb7141b753252c4ddbbbe9eec4eb180a59392f
Tak <env.tak@gmail.com>
2019-12-18 15:53:25 +0900

    refactor: add const value

--- src/app/modules/home/components/svg-face/svg-face.component.ts
+++ src/app/modules/home/components/svg-face/svg-face.component.ts
@@ -3,6 +3,10 @@ import * as Vivus from 'vivus';
 
 import { CheckAnimationService } from '../../../../core/services/check-animation/check-animation.service';
 
+const DRAWING_DURATION = 75;
+const MAX_SCREEN_WIDTH = 1024;
+const EYE_STROKE_WIDTH = '25';
+
 @Component({
     selector: 'prtf-svg-face',
     templateUrl: './svg-face.component.html',
@@ -41,7 +45,7 @@ export class SvgFaceComponent implements OnInit {
             this.doneSvgDrawn();
         };
         const svgFace = new Vivus('avatar', {
-            duration: 75,
+            duration: DRAWING_DURATION,
             type: 'sync',
             animTimingFunction: Vivus.EASE
         }, afterDrawn);
@@ -49,11 +53,11 @@ export class SvgFaceComponent implements OnInit {
 
     private fillEyeColor() {
         this.eyeColor = this.getEyeColor();
-        this.eyeStrokeWidth = '25';
+        this.eyeStrokeWidth = EYE_STROKE_WIDTH;
     }
 
     private getEyeColor() {
-        if (this.screenWidth <= 1024) {
+        if (this.screenWidth <= MAX_SCREEN_WIDTH) {
             return '#bbb';
         }
         return '#3c3c3c';
